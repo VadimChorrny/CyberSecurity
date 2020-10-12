d0c42481b1bd715d5d9b48d50e2aac2d2b7285a8
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-10-12 08:05:04 +0300

    add new script

--- NewUser/script.sh
+++ NewUser/script.sh
@@ -1,87 +1,124 @@
-function userAdd(){
-    read -r -p "Do you want add user? " choice
-     if [[ $choice == y || $chocie -eq yes ]]; then
-         read -r -p "Enter user name: " user
-        useradd $user
-        if [[ $? != 0 ]]; then
-            echo "$?"
-            echo "Eroror user already exists"
-        else 
-            echo "User complited"
-        fi
-     else
-         echo "Good bay"
-     fi
+#!/bin/bash
+function printMenu(){
+
+echo -e "1) Add user"
+echo -e "  a) Name"
+echo -e "  b) name + uid"
+echo -e "  c) add password to user and uid"
+echo -e "2)Delete user"
+echo -e "3) Edit user"
+echo -e "  a) Name"
+echo -e "  b) id"
+echo -e "  c) gid"
+echo -e "4)Create group (with gid)"
+echo -e "5)Add user to group"
+echo -e "6)Drop user from group"
+echo -e "7)Show all users"
+echo -e "0)Exit"
+
 }
-
-function userDelete() {
-    read -r -p 'Do you wand delete user' chocie
-    if [[ $chocie == y || $chocie -eq yes ]]; then
-        read -r -p "Enter user name: " user
-        userdel $user
-        if [[ $? != 0 ]]; then
-        echo "$?"
-        echo "User deleted!"
-        else
-            echo "Error user invalid"
-        fi
-        else
-            echo "bb"
-        fi
+function adduser_name(){
+    read -r -p "Enter user name: " nameuser
+    sudo adduser $nameuser
 }
-
-function changeUser() {
-        read -r -p 'Do you wand change user' chocie
-    if [[ $chocie == y || $chocie -eq yes ]]; then
-        read -r -p "Enter user name: " user
-        usermod -u UID $user
-        read -r -p "Enter user name: " usernick
-        usermod user -l $usernick
+function adduser_name_uid(){
+    read -r -p "Enter user name " nameuser
+    read -r -p "Enter user id" uiduser
+    sudo useradd -u $uiduser $nameuser
 }
-
-function createGroup() {
-    read -r -p 'Do you wand create home group' chocie
-    if [[ $chocie == y || $chocie -eq yes ]]; then
-         vi /etc/sudoers
-         %admin ALL=(ALL) ALL
-         groupadd admin
-        read -r -p "Enter user name: " user
-         useradd $user 
-    echo "user complate"
-    read -r -p 'Do you wand make user adminom?' chocie
-    if [[ $chocie == y || $chocie -eq yes ]]; then
-    usermod -a -G admin TestUser
-    else
-        echo "error"
-    fi
+function add_pass(){
+    read -r -p "Enter user name: " nameuser
+    sudo passwd $nameuser
 }
-
-
-
-function showMenu(){
+function del_user(){
+    read -r -p "Enter user name:" nameuser
+    sudo userdel $nameuser
+}
+function editName(){
+    read -r -p "Enter user name:" newusername
+    read -r -p "Enter old name" oldusername
+    sudo usermod -l $newusername $oldusername
+}
+function editId(){
+    read -r -p "Enter new user id: " newuid
+    read -r -p "Enter user name:" username
+    sudo usermod -u $new_uid $username
+}
+function editGid(){
+    read -r -p "Enter new user id" newgid
+    read -r -p "Enter groupname" groupname
+    sudo groupmod -g $newgid $groupname
+}
+function addGroup(){
+    read -r -p "Enter GID" gid_
+    read -r -p "Enter groupname" groupname
+    sudo addgroup --gid $gid_ $groupname
+}
+function adduserToGroup(){
+    read -r -p "Enter groupname" groupname
+    read -r -p "Enter username: " username
+    sudo usermod -a -G $groupname $username
+}
+function deluserFromGroup(){
+    read -r -p "Enter groupname: " groupname
+    read -r -p "Enter username: " username
+    sudo usermod -R $groupname $username
+}
+function showAllUsers(){
+    cat /etc/passwd | awk -F: '{print $1}'
+}
+exit=false 
+echo -e "---===USER MENU===---"
+while [[ $exit != true ]]
+do
+    printMenu;
+    read -r -p "Enter your answer: " answer
+    case "$answer" in
+    "0")
     exit=true
-    while [[ $exit == true ]]
-    do
-        echo -e "1. Add user\n2.Dell user\n3. Show all user\n 4.Create Home Group\n 5.Change profile users 0. Exit"
-        read item
-        case $item in
-        1) userAdd;
-        ;;
-        2) userDelete;
-        ;;
-        3) cat /etc/passwd
-        ;;
-        4) createGroup;
-        ;;
-        5) changeUser;
-        ;;
-        0) exit=false
-            echo "Good bay";;
-        *) echo "Eroor invalid item"
-            exit 1
-        esac
-    done
-}
-
-
-showMenu;
+    ;;
+    "1")
+    read -r -p "Enter your answer: " answer1
+    if [[ "$answer1" == "a" ]]; then
+    adduser_name;
+    elif [[ "$answer1" == "b" ]]; then
+    editId;
+    elif [[ "$answer1" == "c" ]]; then
+    add_pass;
+    else
+    echo -e "error user invalid"
+    fi
+    ;;
+    "2")
+    del_user;
+    ;;
+    "3")
+    read -r -p "Enter your answer " answer3
+    if [[ "$answer3" == "a" ]]; then
+    editName;
+    elif [[ "$answer3" == "b" ]]; then
+    editGid;
+    elif [[ "$answer3" == "c" ]]; then
+    editGid;
+    else
+    echo -e "error"
+    fi
+    ;;
+    "4")
+    addGroup;
+    ;;
+    "5")
+    adduserToGroup;
+    ;;
+    "6")
+    deluserFromGroup;
+    ;;
+    "7")
+    showAllUsers;
+    ;;
+    *)
+    echo -e "Good Bye"
+    exit 1
+    ;;
+    esac
+done
